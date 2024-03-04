217e6eb4412cfa5be8df21fb0b87fdeaddf6a10c
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-03-04 22:01:58 +0600

    Add: focus style

---
+++ src/lib/style/focus.js
@@ -0,0 +1,9 @@
+/**
+ * This is HTML element focus state common style with normal and error state
+ */
+const style = {
+  base: "focus:outline-dashed focus:outline-2 focus:outline-offset-2 focus:outline-sky-500",
+  error: "focus:outline-red-600",
+};
+
+export { style as focus };
