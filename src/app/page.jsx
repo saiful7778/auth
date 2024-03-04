f4bb199be71d3c5690f7d29c017791eddf8289f0
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-03-04 21:59:29 +0600

    Add: Home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,40 @@
+import Button from "@/components/utilities/Button";
+import Link from "next/link";
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center gap-4 overflow-hidden text-center">
+      <div className="bg-blue-blob absolute -left-96 top-0 z-0 h-[300px] w-[900px] rotate-45 rounded-full blur-[100px] filter"></div>
+      <h1 className="text-6xl font-bold uppercase">Welcome to home page</h1>
+      <p className="text-gray-400">
+        This is simple authentication project build on <br />{" "}
+        <Link className="link" href="https://nextjs.org" target="_blank">
+          next.js
+          <span className="ml-1 inline-block">
+            <FaArrowUpRightFromSquare size={12} />
+          </span>
+        </Link>{" "}
+        and{" "}
+        <Link className="link" href="https://authjs.dev" target="_blank">
+          auth.js
+          <span className="mx-1 inline-block">
+            <FaArrowUpRightFromSquare size={12} />
+          </span>
+          (an authentication package)
+        </Link>
+      </p>
+      <div className="flex items-center justify-center gap-2">
+        <Button variant="primary" href="/authentication/login">
+          Login
+        </Button>
+        <Button variant="primary-outline" href="/authentication/register">
+          Register
+        </Button>
+      </div>
+      <div className="bg-red-blob absolute -right-96 top-28 z-0 h-[300px] w-[900px] -rotate-[20deg] rounded-full blur-[100px] filter"></div>
+    </div>
+  );
 };
 
 export default HomePage;
